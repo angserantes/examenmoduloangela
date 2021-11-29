26675bda859d78283be8e5fa222fa56b73075a21
angeserantes <aserantes7@gmail.com>
2021-11-29 11:52:47 +0100

    cambios examen

---
+++ js/footer.js
@@ -0,0 +1,87 @@
+const footerTemplate = document.createElement('template');
+
+footerTemplate.innerHTML = `
+  <style>
+    footer {
+      height: 60px;
+      padding: 0 10px;
+      list-style: none;
+      display: flex;
+      flex-shrink: 0;
+      justify-content: space-between;
+      align-items: center;
+      background-color: #dfdfe2;
+    }
+
+    ul {
+      padding: 0;
+    }
+    
+    ul li {
+      list-style: none;
+      display: inline;
+    }
+    
+    a {
+      margin: 0 15px;
+      color: inherit;
+      text-decoration: none;
+    }
+    
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -2px 0 0 #333;
+    }
+    
+    .social-row {
+      font-size: 20px;
+    }
+    
+    .social-row li a {
+      margin: 0 15px;
+    }
+  </style>
+  <footer>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="nos.html">Acerca de nós</a></li>
+        <li><a href="servizos.html">Nosos servizos</a></li>
+        <li><a href="contacto.html">Contacto</a></li>
+    </ul>
+    <ul class="social-row">
+    <li class="icon-item">
+                        <a href="#" class="icon-link"><i class="fab fa-facebook-f"></i></a>
+                    </li>
+                    <li class="icon-item">
+                        <a href="#" class="icon-link"><i class="fab fa-youtube"></i></a>
+                    </li>
+                    <li class="icon-item">
+                        <a href="#" class="icon-link"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li class="icon-item">
+                        <a href="#" class="icon-link"><i class="fab fa-google-plus-g"></i></a>
+                    </li>
+    </ul>
+  </footer>
+`;
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const fontAwesome = document.querySelector('link[href*="font-awesome"]');
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+    if (fontAwesome) {
+      shadowRoot.appendChild(fontAwesome.cloneNode());
+    }
+
+    shadowRoot.appendChild(footerTemplate.content);
+  }
+}
+
+customElements.define('footer-component', Footer);
+  
+  customElements.define('footer-component', Footer);
---
+++ readme.md
@@ -0,0 +1,10 @@
+Examen de módulo Ángela.
+
+Abrimos Laragon y ordenamos el proyecto.
+Procedemos a añadir el repositorio a GitHub mediante el comando Git init.
+
+Damos forma a los html mediante Emmet. VInculamos hoja de estilos de css, fuente y foundawesome en el head. En la parte final del body vinculamos los header y footer de javasript, en las que en ambas daremos forma a los menús de los mismos, de esta forma no es necesario utilizar el html, ni el css para crear un menú.
+
+Cubrimos los campos de los html, entre ellos destacan el formulario de inscripción y las tarjetas de presentación del equipo.
+
+Damos forma al contenido de todos los html en la misma hoja de estilos a través de diferentes clases.
